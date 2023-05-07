ce717f5c84aae99e27d82ddeeea7aa7d7878e8fc
Thomas Belhalfaoui <belhalfaoui@gmail.com>
2023-05-07 13:11:40 +0200

    Numeric fields working

--- frontend/src/components/TaxBox.js
+++ frontend/src/components/TaxBox.js
@@ -1,15 +1,22 @@
 import { NumericFormat } from "react-number-format";
 
 
-export const TaxBox = ({box, onChange, modifyTotals}) => (
+export const NumberBox = ( props ) => (
+    <NumericFormat class="form-control"
+        thousandSeparator=" "
+        decimalScale={0}
+        isAllowed={(val) => (props.max) ? ((val.floatValue <= props.max) || (val.value == "")) : true}
+        defaultValue=""
+        {...props} />
+)
+
+export const TaxBox = ({box, onValueChange, onSliderChange, unlockTotals}) => (
     <div class="row">
         <div class="col-1">
-            <NumericFormat class="form-control" name={`${box.code}.raw_value`} value={box.raw_value}
-            thousandSeparator=" " disabled={!modifyTotals} onChange={onChange} />
+            <NumberBox name={`${box.code}.raw_value`} value={box.raw_value} onValueChange={onValueChange} disabled={!unlockTotals}  />
         </div>
         <div class="col-1">
-            <NumericFormat class="form-control" name={`${box.code}.partner_0_value`} value={box.partner_0_value}
-            thousandSeparator=" " onChange={onChange} min="0" max={box.raw_value}/>
+            <NumberBox name={`${box.code}.partner_0_value`} value={box.partner_0_value} max={box.raw_value} onValueChange={onValueChange} />
         </div>
         <div class="col-2">
             <input type="range" class="form-range" name={`${box.code}.slider`}
@@ -17,11 +24,10 @@ export const TaxBox = ({box, onChange, modifyTotals}) => (
             step={(box.raw_value <= 10) ? 1 : Math.round(box.raw_value / 10)}
             disabled={box.raw_value == ""}
             value={(box.raw_value != "") ? (1 - box.ratio_0) * box.raw_value : ""}
-            onChange={onChange} />
+            onChange={onSliderChange} />
         </div>
         <div class="col-1">
-            <NumericFormat min="0" max={box.raw_value} class="form-control" 
-            thousandSeparator=" " name={`${box.code}.partner_1_value`} value={box.partner_1_value} onChange={onChange} />
+            <NumberBox name={`${box.code}.partner_1_value`} value={box.partner_1_value} max={box.raw_value} onValueChange={onValueChange} />
         </div>
         <div class="col">
             <label for={`${box.code}.raw_value`} class="form-label">{box.code} - {box.description}</label>
--- frontend/src/components/TaxBoxesPanel.js
+++ frontend/src/components/TaxBoxesPanel.js
@@ -3,120 +3,89 @@ import { TaxBox } from './TaxBox.js'
 
 export const TaxBoxesPanel = ({boxes, setBoxes}) => {
 
-    const [modifyTotals, setModifyTotals] = useState(false);
+    const [ showAutofilled, setShowAutoFilled ] = useState(false);
+    const [ unlockTotals, setUnlockTotals ] = useState(false);
 
-    const onBoxChange = async (evt) => {
-        let boxCode, fieldName;
-        [boxCode, fieldName] = evt.target.name.split('.');
-
-        let boxesNew = { ...boxes };
-        var value = parseInt(evt.target.value.replace(' ', ''));
-
-        if (fieldName == 'slider') {
-            boxesNew[boxCode].ratio_0 = 1 - value / boxesNew[boxCode].raw_value;
-            console.log(boxesNew[boxCode].raw_value);
-        }
-        else if (isNaN(value)) {
+    const onSliderChange = async (evt) => {
+        var value = evt.target.value;
+        const boxCode = evt.target.name.split('.')[0];
+        if (isNaN(value)) {
             value = "";
         }
-        boxesNew[boxCode][fieldName] = value;
+        let boxesNew = { ...boxes };
+        boxesNew[boxCode].ratio_0 = 1 - value / boxesNew[boxCode].raw_value;
+        boxesNew[boxCode]['partner_0_value'] = Math.round(boxesNew[boxCode]['ratio_0'] * boxesNew[boxCode]['raw_value']);
+        boxesNew[boxCode]['partner_1_value'] = Math.round((1 - boxesNew[boxCode]['ratio_0']) * boxesNew[boxCode]['raw_value']);
+        setBoxes(boxesNew);
+    }
+
+    const onBoxChange = async (values, sourceInfo) => {
+        const evt = sourceInfo.event;
+        const [boxCode, fieldName] = evt.target.name.split('.');
+
+        let boxesNew = { ...boxes };
+        boxesNew[boxCode][fieldName] = values.floatValue;
         
         if (fieldName == 'partner_0_value') {
-            if (!modifyTotals) {
+            if (!unlockTotals) {
                 boxesNew[boxCode]['partner_1_value'] = Math.round(boxesNew[boxCode]['raw_value'] - boxesNew[boxCode]['partner_0_value']);
             }
             else {
                 boxesNew[boxCode]['partner_1_value'] = Math.round((1 - boxesNew[boxCode]['ratio_0']) * boxesNew[boxCode]['raw_value']);
                 boxesNew[boxCode]['raw_value'] = Math.round(boxesNew[boxCode]['partner_0_value'] + boxesNew[boxCode]['partner_1_value']);
             }
-            boxesNew[boxCode]['ratio_0'] = Math.round(boxesNew[boxCode]['partner_0_value'] / boxesNew[boxCode]['raw_value']);
+            boxesNew[boxCode]['ratio_0'] = boxesNew[boxCode]['partner_0_value'] / boxesNew[boxCode]['raw_value'];
         }
         else if (fieldName == 'partner_1_value') {
-            if (!modifyTotals) {
+            if (!unlockTotals) {
                 boxesNew[boxCode]['partner_0_value'] = Math.round(boxesNew[boxCode]['raw_value'] - boxesNew[boxCode]['partner_1_value']);
             }
             else {
                 boxesNew[boxCode]['partner_0_value'] = Math.round(boxesNew[boxCode]['ratio_0'] * boxesNew[boxCode]['raw_value']);
                 boxesNew[boxCode]['raw_value'] = Math.round(boxesNew[boxCode]['partner_0_value'] + boxesNew[boxCode]['partner_1_value']);
             }
-            boxesNew[boxCode]['ratio_0'] = Math.round(boxesNew[boxCode]['partner_0_value'] / boxesNew[boxCode]['raw_value']);
-        }
-        if (fieldName == 'slider') {
-            boxesNew[boxCode]['partner_0_value'] = Math.round(boxesNew[boxCode]['ratio_0'] * boxesNew[boxCode]['raw_value']);
-            boxesNew[boxCode]['partner_1_value'] = Math.round((1 - boxesNew[boxCode]['ratio_0']) * boxesNew[boxCode]['raw_value']);
+            boxesNew[boxCode]['ratio_0'] = boxesNew[boxCode]['partner_0_value'] / boxesNew[boxCode]['raw_value'];
         }
         if (fieldName == 'raw_value') {
             boxesNew[boxCode]['partner_0_value'] = Math.round(boxesNew[boxCode]['ratio_0'] * boxesNew[boxCode]['raw_value']);
             boxesNew[boxCode]['partner_1_value'] = Math.round((1 - boxesNew[boxCode]['ratio_0']) * boxesNew[boxCode]['raw_value']);
-        }
-        
+        }        
         setBoxes(boxesNew);
     };
 
-    const toggleModifyTotals = async (evt) => {
-        setModifyTotals(evt.target.checked);
+    const toggleShowAutofilled = async (evt) => {
+        setShowAutoFilled(evt.target.checked);
     };
 
-    const boxesUnattributed = Object.values(boxes).filter(
-        box => box.ratio_0 == null
-    );
-
-    const boxesAttributed = Object.values(boxes).filter(
-        box => box.ratio_0 != null
-    );
+    const toggleUnlockTotals = async (evt) => {
+        setUnlockTotals(evt.target.checked);
+    };
 
     return (
-    <form>
-        <div id="containerStep2" class="container py-2 text-start">
-            <div class="form-check form-switch py-2">
-                <input class="form-check-input" type="checkbox" role="switch" id="modifyTotals" checked={modifyTotals} onChange={toggleModifyTotals} />
-                <label class="form-check-label" for="modifyTotals">Modifier les totaux</label>
-            </div>
-            <div class="accordion" id="accordionBoxes">
-                {boxesUnattributed.length > 0 && (
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingManualBoxes">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseManualBoxes" aria-expanded="true" aria-controls="collapseManualBoxes">
-                            Cases communes, à attribuer manuellement.
-                            </button>
-                        </h2>
-                        <div id="collapseManualBoxes" class="accordion-collapse collapse show"
-                        aria-labelledby="headingManualBoxes" data-bs-parent="#accordionBoxes">
-                            <div class="accordion-body">
-                                {boxesUnattributed.map((box) => (
-                                    <TaxBox box={box} onChange={onBoxChange} modifyTotals={modifyTotals} />
-                                ))}
-                            </div>
+        <form>
+            <div id="containerStep2" class="container py-2 text-start">
+                <div>
+                    <div>
+                        <div class="form-check form-switch py-2">
+                            <input class="form-check-input" type="checkbox" role="switch" id="showAutofilled" checked={showAutofilled} onChange={toggleShowAutofilled} />
+                            <label class="form-check-label" for="showAutofilled">Afficher toutes les cases, même celles déjà individualisées.</label>
                         </div>
-                    </div>
-                )}
-                {boxesAttributed.length > 0 && (
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingAutoBoxes">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseAutoBoxes" aria-expanded="false" aria-controls="collapseAutoBoxes">
-                            Cases séparées, attribuées automatiquement.
-                            </button>
-                        </h2>
-                        <div id="collapseAutoBoxes" class="accordion-collapse collapse" aria-labelledby="headingAutoBoxes"
-                        data-bs-parent="#accordionBoxes">
-                            <div class="accordion-body">
-                            <div class="row text-center">
-                                <div class="col-1"><h6>Total</h6></div>
-                                <div class="col-1"><h6>Déclarant·e 1</h6></div>
-                                <div class="col-2"><h6>Répartition</h6></div>
-                                <div class="col-1"><h6>Déclarant·e 2</h6></div>
-                            </div>
-                                {boxesAttributed.map(box => (
-                                    <TaxBox box={box} onChange={onBoxChange} modifyTotals={modifyTotals} />
-                                ))}
-                            </div>
+                        <div class="form-check form-switch py-2">
+                            <input class="form-check-input" type="checkbox" role="switch" id="unlockTotals" checked={unlockTotals} onChange={toggleUnlockTotals} />
+                            <label class="form-check-label" for="unlockTotals">Déverrouiller les totaux.</label>
                         </div>
+                        <div class="row text-center">
+                            <div class="col-1"><h6>Total</h6></div>
+                            <div class="col-1"><h6>Déclarant·e 1</h6></div>
+                            <div class="col-2"><h6>Répartition</h6></div>
+                            <div class="col-1"><h6>Déclarant·e 2</h6></div>
+                        </div>
+                            {Object.values(boxes).map(box => (
+                                <TaxBox box={box} onValueChange={onBoxChange} onSliderChange={onSliderChange} unlockTotals={unlockTotals} />
+                            ))}
                     </div>
-                )}
+                </div>
             </div>
-        </div>
-    </form>
+        </form>
 );
 }
