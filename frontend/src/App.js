d9a3f98fc6098cedba64a0673ad569b2b6251f54
Thomas Belhalfaoui <belhalfaoui@gmail.com>
2023-05-12 20:36:34 +0200

    Fix step type

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,16 +18,16 @@ function App() {
         <Header />
         <Accordion>
           <AccordionStepItem title="1. Sélectionnez le fichier PDF de votre déclaration commune."
-          itemStep="1" currentStep={step} maxCurrentStep={maxStep} setStep={setStep}>
+          itemStep={1} currentStep={step} maxCurrentStep={maxStep} setStep={setStep}>
             <PdfSubmitForm setBoxes={setBoxes} setStep={(s) => setStep(s) || setMaxStep(s)} />
           </AccordionStepItem>
           <AccordionStepItem title="2. Vérifiez l'attribution des cases à chaque déclarant·e."
-          itemStep="2" currentStep={step} maxCurrentStep={maxStep} setStep={setStep}>
+          itemStep={2} currentStep={step} maxCurrentStep={maxStep} setStep={setStep}>
             <TaxBoxesPanel boxes={boxes} setBoxes={setBoxes} setStep={(s) => setStep(s) || setMaxStep(s)}
             setIndividualizedResults={setIndividualizedResults} />
           </AccordionStepItem>
           <AccordionStepItem title="3. Résultats: l'impôt individualisé!"
-          itemStep="3" currentStep={step} maxCurrentStep={maxStep} setStep={setStep}>
+          itemStep={3} currentStep={step} maxCurrentStep={maxStep} setStep={setStep}>
             <ResultsPanel results={individualizedResults} />
           </AccordionStepItem>
         </Accordion>
