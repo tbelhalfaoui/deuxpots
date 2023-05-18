f29a7cfeaefd97a98c54dd3e7299f44b58de0671
Thomas Belhalfaoui <belhalfaoui@gmail.com>
2023-05-18 14:47:34 +0200

    Use faCircleInfo icon instead of svg

--- frontend/src/components/PdfSubmitForm.js
+++ frontend/src/components/PdfSubmitForm.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react"
 import { ErrorMessage } from "./ErrorMessage.js";
 
@@ -58,10 +60,7 @@ export const PdfSubmitForm = ({setBoxes, setStep, isError}) => {
         <form onSubmit={sendTaxSheet}>
           <ErrorMessage error={errorMsg} />
           <div class="alert alert-primary" role="alert">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-              </svg> Aucune donnée issue de votre déclaration d'impôt ne sera collectée.<br/>
+              <FontAwesomeIcon icon={faCircleInfo} /> Aucune donnée issue de votre déclaration d'impôt ne sera collectée.<br/>
               Seuls les montants, anonymes, seront utilisés temporairement pour faire la simulation.
           </div>
           <div class="container py-4 text-center" id="containerStep1">
