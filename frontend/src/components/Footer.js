59e0cff85ac11a86c4ca6bd28fe4e54b0f5be8f0
Thomas Belhalfaoui <belhalfaoui@gmail.com>
2023-05-21 21:30:08 +0200

    Separate footer and add contact e-mail address

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+export const Footer = () => (
+    <div className="container text-center pt-4">
+        <div className="row">
+            <div className="col-xl-6 py-1 py-xl-0">
+                <a className="footerLink" href="https://github.com/tbelhalfaoui/deuxpots">
+                    Le code source de ce site est ouvert : https://github.com/tbelhalfaoui/deuxpots
+                </a>
+            </div>
+            <div className="footerLink col-xl-6 py-1 py-xl-0">
+                Toute remarque est la bienvenue&nbsp;: contact@deuxpots.fr
+            </div>
+        </div>
+    </div>
+)
