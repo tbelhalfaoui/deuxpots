ddd8dc17f8e2a0a5864b3b74e8a4ee2792f061ca
Thomas Belhalfaoui <belhalfaoui@gmail.com>
2023-05-21 16:22:34 +0200

    Update header text

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,23 @@
 export const Header = () => (
     <div className="jumbotron">
       <h1 className="display-4">Impôt, deux pots</h1>
-      <p className="lead">
-        <i>Deux pots</i> est un outil simple pour offrir le choix à chacun·e qui le souhaite d'indivisualiser son impôt.
+      <p>
+        En 2023, en France, les couples mariés ou pacés doivent faire une déclaration d'impôt commune.
+        L'administration fiscale réclame ou reverse ensuite une somme globale au couple, sans distinguer
+        la part de chacun·e.
+      </p>
+      <p>
+        Ce fonctionnement peut léser les personnes qui ont choisi de conserver une indépendance
+        financière au sein du couple, en particulier si les différences de revenus sont importantes.
       </p>
       <div>
         <p>
-          Lorsqu'on est marié ou pacsé, la déclaration d'impôt commune est obligatoire. Certes, lorsque les deux personnes
-          sont salariées, il est possible d'individualiser le prélèvement à la source, mais cela n'est pas suffisant&nbsp;:
+          Le prélèvement à la source individualisé est un début de réponse, mais qui ne s'applique pas à toutes
+          les situations (travailleur·euse·s indépendant·e·s, forte variation de revenus d'une année sur l'autre, etc.).
         </p>
-        <ul>
-          <li>Les personnes non-salariées ne sont pas concernées par le prélèvement à la source.</li>
-          <li>Le prélèvement à la source n'est jamais exact : il y a toujours un reliquat à payer ou un trop-perçu 
-            à récupérer en fin d'année. Comment le partager entre les deux partenaires ?</li>
-        </ul>
-        
+        <p className="lead">
+          <i>Deux pots</i> est un outil simple pour offrir le choix à chacun·e d'individualiser son impôt au sein du couple.
+        </p>
         <p>
           Concrètement, il suffit de soumettre le fichier PDF de la déclaration d'impôt commune, et l'outil indique qui 
           doit payer (ou récupérer) quel montant.
