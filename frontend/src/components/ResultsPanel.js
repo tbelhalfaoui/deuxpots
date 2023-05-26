f43760d5832f6d8f2da830b41bef5ca21aca3a5b
Thomas Belhalfaoui <belhalfaoui@gmail.com>
2023-05-26 09:39:42 +0200

    WIP visualization

--- frontend/src/components/ResultsPanel.js
+++ frontend/src/components/ResultsPanel.js
@@ -1,3 +1,6 @@
+import Plot from 'react-plotly.js';
+
+
 export const ResultRow = ({ label, fieldName, partnerIndex, style, results }) => (
     <div className="py-1 row" style={style}>
         <div className="col-lg-8">
@@ -37,6 +40,75 @@ export const ResultCard = ({ results, partnerIndex }) =>
 
 export const ResultsPanel = ({ results }) => (
     <div>
+        <div className="row">
+            <div className="col-5">
+                <Plot
+                    data={[
+                    {
+                        x: [-1000],
+                        y: ["montant"],
+                        name: "",
+                        type: 'bar',
+                        orientation: 'h',
+                        marker: {color: 'rgba(140, 169, 255, 1.0)'},
+                        hovertemplate: "[1] Reste à payer: 1 000 €"
+                    },
+                    {
+                        x: [-4000],
+                        y: ["montant"],
+                        name: "",
+                        type: 'bar',
+                        orientation: 'h',
+                        marker: {color: 'rgba(138, 169, 237, .4)'},
+                        hovertemplate: "[1] Déjà payé: 2 000 €"
+                    },
+                    {
+                        x: [-2000],
+                        y: ["montant"],
+                        name: "",
+                        type: 'bar',
+                        orientation: 'h',
+                        marker: {color: 'rgba(0, 0, 0, .1)'},
+                        hovertemplate: "[1] Gain par rapport à une déclaration séparée: 2 000 €"
+                    },
+                    
+                    {
+                        x: [1000],
+                        y: ["montant"],
+                        name: "",
+                        type: 'bar',
+                        orientation: 'h',
+                        marker: {color: 'rgba(140, 169, 255, 1.0)'},
+                        hovertemplate: "[2] Reste à payer: 1 000 €"
+                    },
+                    {
+                        x: [4000],
+                        y: ["montant"],
+                        name: "",
+                        type: 'bar',
+                        orientation: 'h',
+                        marker: {color: 'rgba(138, 169, 237, .4)'},
+                        hovertemplate: "[2] Déjà payé: 2 000 €"
+                    },
+                    {
+                        x: [2000],
+                        y: ["montant"],
+                        name: "",
+                        type: 'bar',
+                        orientation: 'h',
+                        marker: {color: 'rgba(0, 0, 0, .1)'},
+                        hovertemplate: "[2] Gain par rapport à une déclaration séparée: 2 000 €"
+                    }
+                    ]}
+                    layout={{
+                        width:'100%', height: 80, barmode: 'relative', showlegend: false,
+                        margin: {l: 0, r: 0, b: 0, t: 0, pad: 0},
+                        xaxis: {tickvals: []}, yaxis: {tickvals: []}
+                    }}
+                    config={{displayModeBar: false}}
+                />
+            </div>
+        </div>
         <div className="row justify-content-center">
             <div className="p-3 col-md-6 col-xl-5">
                 <ResultCard results={results} partnerIndex={0} />
