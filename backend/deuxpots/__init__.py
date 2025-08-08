d134215ff5febfd96f6caf8052cf03906c04a73a
Victor R. <v.raimbourg@gmail.com>
2025-08-08 10:35:26 +0200

    update SIMULATOR_URL to 2025 version

--- backend/deuxpots/__init__.py
+++ backend/deuxpots/__init__.py
@@ -8,4 +8,4 @@ DEV_MODE = not environ.get('FLY_APP_NAME')
 CATEGORY_COORDS_PATH = ROOT_PATH / "resources/category_coords.json"
 CERFA_VARIABLES_PATH = ROOT_PATH / "resources/cerfa_variables.json"
 
-SIMULATOR_URL = "https://simulateur-ir-ifi.impots.gouv.fr/cgi-bin/calc-2024.cgi"
+SIMULATOR_URL = "https://simulateur-ir-ifi.impots.gouv.fr/cgi-bin/calc-2025.cgi"
