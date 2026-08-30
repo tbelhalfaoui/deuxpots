587b2efea3866612c00ad689690ff6758d0277f5
Thomas Belhalfaoui <belhalfaoui@gmail.com>
2026-08-30 23:50:13 +0200

    Add Stripe donate button

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,19 @@ import { LegalNotice } from "./LegalNotice";
 
 export const Footer = () => {
     return <div className="container text-center pt-4">
-        <div className="row">
+        <div className="row footerLink">
+                Je développe ce site bénévolement et prends en compte vos retours avec plaisir,
+                pour le faire fonctionner dans toutes les situations (et elles sont nombreuses !...).<br/>
+                Alors s'il vous a été utile et si vous pouvez me soutenir financièrement, je vous en serais très reconnaissant :)
+        </div>
+        <div className="row footerLink py-1">
+                <stripe-buy-button
+                buy-button-id="buy_btn_1UAGAdC8zu98Ytw3yHJBd39N"
+                publishable-key="pk_live_51UAFbxC8zu98Ytw3REVyjtMn9v5xOGBj0GrxYbwngmgAH9yrkJZzvuDqtrvec7hA40tIWnTBMgVDvnWzWaT2XuNB00uk6NJ0VU"
+                >
+                </stripe-buy-button>
+        </div>
+        <div className="row py-3">
             <div className="col-xl-5 py-1 py-xl-0">
                 <a href="https://github.com/tbelhalfaoui/deuxpots" className="footerLink" target="_blank" rel="noreferrer">
                     Le code source de ce site est ouvert <FaGithub style={{color: 'gray'}} />
