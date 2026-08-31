f3e225858ce21c23981fedbbc755242c1f31c921
Thomas Belhalfaoui <belhalfaoui@gmail.com>
2026-08-31 20:45:57 +0200

    Static version of the SIMULATOR_URL auto-resolve

--- backend/deuxpots/__init__.py
+++ backend/deuxpots/__init__.py
@@ -1,6 +1,7 @@
-from datetime import date
 from os import environ
 from pathlib import Path
+from datetime import date
+import requests as rq
 
 ROOT_PATH = Path(__file__).parent.parent.absolute()
 
@@ -9,19 +10,17 @@ DEV_MODE = not environ.get('FLY_APP_NAME')
 CATEGORY_COORDS_PATH = ROOT_PATH / "resources/category_coords.json"
 CERFA_VARIABLES_PATH = ROOT_PATH / "resources/cerfa_variables.json"
 
-_SIMULATOR_URL_TEMPLATE = "https://simulateur-ir-ifi.impots.gouv.fr/cgi-bin/calc-{year}.cgi"
-_confirmed_urls = {}  # year -> url, only populated on successful HEAD
+SIMULATOR_URL_TEMPLATE = "https://simulateur-ir-ifi.impots.gouv.fr/cgi-bin/calc-{year}.cgi"
 
 
-def get_simulator_url():
-    import requests
+def _make_simulator_url():
     current_year = date.today().year
-    if current_year not in _confirmed_urls:
-        url = _SIMULATOR_URL_TEMPLATE.format(year=current_year)
-        try:
-            resp = requests.head(url, timeout=5)
-            if resp.status_code < 400:
-                _confirmed_urls[current_year] = url
-        except requests.RequestException:
-            pass
-    return _confirmed_urls.get(current_year, _SIMULATOR_URL_TEMPLATE.format(year=current_year - 1))
+    try:
+        url = SIMULATOR_URL_TEMPLATE.format(year=current_year)
+        rq.head(url, timeout=5).raise_for_status()
+        return url
+    except rq.RequestException:
+        return SIMULATOR_URL_TEMPLATE.format(year=current_year - 1)
+
+
+SIMULATOR_URL = _make_simulator_url()
